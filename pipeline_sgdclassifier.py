36c63e57c208aed797fb80445bb406ed17b7b5fe
ecdiazl <ecdiazl@icloud.com>
2024-01-27 20:50:48 -0300

    Segundo componente creado y compilado

--- pipeline_sgdclassifier.py
+++ pipeline_sgdclassifier.py
@@ -17,7 +17,7 @@ DATASET_ID = "data_mlops_fp"
 TABLE_ID = "tb_mlops_fp"  
 
 @component(
-            packages_to_install=["google-cloud-bigquery==3.10.0"],
+            packages_to_install=["google-cloud-bigquery[pandas]==3.10.0"],
 )
 def read_bigquery_table(
     project_id: str, 
@@ -53,6 +53,43 @@ def read_bigquery_table(
     df = query_job.result().to_dataframe()
     df.to_csv(dataset.path, index=False)
 
+@component(
+    packages_to_install=[
+        "pandas==1.3.5",
+        "scikit-learn==1.0.2",
+    ],
+)
+def data_preprocessing(
+    dataset: Input[Dataset],
+    train_dataset: Output[Dataset],
+    test_dataset: Output[Dataset],
+):
+    """Preprocesses tabular data for training."""
+    import pandas as pd
+    from sklearn.model_selection import train_test_split
+    from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+    # lee los datos de entrada
+    df = pd.read_csv(dataset.path)
+
+    # separamos el target del resto de las variables
+    X, y = df.iloc[:,1:-1], df[['target']]
+    columns_x = X.columns
+
+    # escalamos las variables
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    X = pd.DataFrame(X, columns=columns_x)
+
+    # concatenamos las variables escaladas con el target
+    df_scaled = pd.concat([X, y], axis=1)
+
+    # Split the data into train and test sets.
+    train_df, test_df = train_test_split(df_scaled, test_size=0.2, random_state=42)
+
+    train_df.to_csv(train_dataset.path, index=False)
+    test_df.to_csv(test_dataset.path, index=False)
+
 
 @dsl.pipeline(
     name="mlops-fp-pipeline",
@@ -64,6 +101,14 @@ def pipeline():
         project_id=PROJECT_ID,
         dataset_id=DATASET_ID,
         table_id=TABLE_ID,
+    )
+
+    data_preprocessing_task = (
+        data_preprocessing(
+            dataset=read_bigquery_table_task.outputs["dataset"],
+        )
+        .after(read_bigquery_table_task)
+        .set_caching_options(False)
     )
     
 if __name__ == '__main__':
