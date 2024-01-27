72904f5f7b6afe90721f00ec1770e968d7c1bb0d
ecdiazl <ecdiazl@icloud.com>
2024-01-27 12:27:30 -0300

    Primer objeto compilado

--- pipeline_sgdclassifier.py
+++ pipeline_sgdclassifier.py
@@ -1,4 +1,8 @@
-from kfp.v2.dsl import component
+import kfp
+from kfp import dsl
+from kfp.v2 import compiler
+from kfp.v2.dsl import (Artifact, Dataset, Input, InputPath, Model, Output,
+                        OutputPath, ClassificationMetrics, Metrics, component, pipeline)
 
 PROJECT_ID = 'mlops-final-project-412223'
 DATASET_ID = "data_mlops_fp"  
@@ -35,3 +39,23 @@ def read_bigquery_table(
 
     # Convierte los resultados en un dataframe de pandas y devuelve una representación en cadena.
     df = query_job.to_dataframe()
+
+
+@dsl.pipeline(
+    name="mlops-fp-pipeline",
+)
+def pipeline():
+    """A demo pipeline."""
+
+    read_bigquery_table_task = read_bigquery_table(
+        project_id=PROJECT_ID,
+        dataset_id=DATASET_ID,
+        table_id=TABLE_ID,
+    )
+    
+if __name__ == '__main__':
+    
+    compiler.Compiler().compile(
+        pipeline_func=pipeline, package_path="tab_classif_pipeline.json"
+    )
+    print('Pipeline compilado exitosamente')
